7a478f1e2ec9c156f03ff2800f80a896a3f92638
Elina Mangal <55766712+mangalelina@users.noreply.github.com>
2024-12-12 17:25:17 -0600

    Create longestSubstringWithoutRepeatingCharacters_3.cpp

---
+++ longestSubstringWithoutRepeatingCharacters_3.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> dict;
+        int left = 0, right = 0, ans = 0;
+        for (right = 0; right < s.size(); right++) {
+            
+        }
+
+    }
+};
